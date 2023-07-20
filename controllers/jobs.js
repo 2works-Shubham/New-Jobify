aafe192ba6a60e36765c31ee10c266defe08206c
2works-Shubham <shubham@2works.io>
2023-07-20 18:17:52 +0530

    sec58.v345.Monthly applications-server

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -100,7 +100,40 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   };
 
-  let monthlyApplications = []; 
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $sort: { "_id.year": -1, "_id.month": -1 },
+    },
+    {
+      $limit: 6,
+    },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      // accepts 0-11
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
@@ -238,20 +271,20 @@ export { createJob, deleteJob, getAllJobs, updateJob, showStats };
 //     { $limit: 6 },
 //   ]);
 
-//   monthlyApplications = monthlyApplications
-//     .map((item) => {
-//       const {
-//         _id: { year, month },
-//         count,
-//       } = item;
-//       // accepts 0-11
-//       const date = moment()
-//         .month(month - 1)
-//         .year(year)
-//         .format("MMM Y");
-//       return { date, count };
-//     })
-//     .reverse();
+// monthlyApplications = monthlyApplications
+//   .map((item) => {
+//     const {
+//       _id: { year, month },
+//       count,
+//     } = item;
+//     // accepts 0-11
+//     const date = moment()
+//       .month(month - 1)
+//       .year(year)
+//       .format("MMM Y");
+//     return { date, count };
+//   })
+//   .reverse();
 
 //   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 // };
