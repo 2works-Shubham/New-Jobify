a4aefd27b3fb0f2d8419516aee21f95b381dbfe3
2works-Shubham <shubham@2works.io>
2023-07-27 16:47:42 +0530

    sec66.Security packages and extra content

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,19 @@
-import rateLimiter from 'express-rate-limit'
-const apiLimiter = rateLimiter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
-})
 
 import express from 'express'
 const router = express.Router()
+
+import rateLimiter from "express-rate-limit"
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: 'Too many requests from this IP address, please try again after 15 minutes.',
+})
 import { register, login, updateUser } from '../controllers/auth.js'
 import authenticateUser from '../middleware/authentication.js'
 
 router.post('/register', apiLimiter, register)
 router.post('/login', apiLimiter, login)
 router.patch('/updateUser', authenticateUser, updateUser)
+
 export default router
